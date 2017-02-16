84c6d611563c19a8c19c2cbb2d0c5ce70f017502
Bastian <kueppers@itc.rwth-aachen.de>
2017-02-16 14:24:00 +0100

    Added "webview-ready" Event This event is fired after the "did-finish-load" event of the webview of a tab was fired.

--- index.js
+++ index.js
@@ -294,6 +294,13 @@ const TabPrivate = {
 
     initWebview: function () {
         this.webview = document.createElement("webview");
+        
+        const tabWebviewDidFinishLoadHandler = function (e) {
+            this.emit("webview-ready", this);
+        };
+
+        this.webview.addEventListener("did-finish-load", tabWebviewDidFinishLoadHandler.bind(this), false);
+        
         this.webview.classList.add(this.tabGroup.options.viewClass);
         if (this.webviewAttributes) {
             let attrs = this.webviewAttributes;
@@ -301,6 +308,7 @@ const TabPrivate = {
                 this.webview.setAttribute(key, attrs[key]);
             }
         }
+        
         this.tabGroup.viewContainer.appendChild(this.webview);
     }
 };
