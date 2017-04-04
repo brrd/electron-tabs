218449d56af92434fa5871506c00ab16c5af32c7
Robin Riclet <robinriclet@gmail.com>
2017-04-04 15:37:53 +0200

    Restored whitespaces

--- index.js
+++ index.js
@@ -295,13 +295,13 @@ const TabPrivate = {
 
     initWebview: function () {
         this.webview = document.createElement("webview");
-
+        
         const tabWebviewDidFinishLoadHandler = function (e) {
             this.emit("webview-ready", this);
         };
 
         this.webview.addEventListener("did-finish-load", tabWebviewDidFinishLoadHandler.bind(this), false);
-
+        
         this.webview.classList.add(this.tabGroup.options.viewClass);
         if (this.webviewAttributes) {
             let attrs = this.webviewAttributes;
@@ -309,7 +309,7 @@ const TabPrivate = {
                 this.webview.setAttribute(key, attrs[key]);
             }
         }
-
+        
         this.tabGroup.viewContainer.appendChild(this.webview);
     }
 };
